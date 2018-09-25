cead6e12dfaf8e4cb59fa510ca6aeb2af403e6c1
ShakedBraimok <shaked1817@gmail.com>
2018-09-25 10:30:51 +0300

    test

--- listener/listener.py
+++ listener/listener.py
@@ -12,3 +12,6 @@ def get_hook():
     os.system("docker pull shakedbraimok/example-app && docker stop example-app && docker rm example-app && docker run --name example-app -d --expose 8080 -p 127.0.0.1:8080:8080 -i -t shakedbraimok/example-app /bin/bash")
 
 
+if __name__ == '__main__':
+   app.run()
+   
