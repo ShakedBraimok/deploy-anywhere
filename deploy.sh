27ea883346d5bbe40a90b25b32a202d72bf16290
ShakedBraimok <shaked1817@gmail.com>
2018-09-26 12:14:03 +0300

    folder reorganized

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,6 @@ docker swarm init
 
 docker service create --name jenkins-master  -p 50000:50000 -p 80:8080 jenkinsci/jenkins:latest
 
-#--- AGENT ---#
-echo "-master http://localhost -password admin -username admin"|docker secret create jenkins-v1 -
-
-docker service create \
-	--mode=global \
-	--name jenkins-swarm-agent \
-	-e LABELS=docker-test \
-	--mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" \
-	--mount "type=bind,source=/tmp/,target=/tmp/" \
-	--secret source=jenkins-v1,target=jenkins \
-	vipconsult/jenkins-swarm-agent
-
 #--- cp Pipeline files to the container (jenkins-master) in the path /opt/deployment-files ---#
 containerId = $(docker ps -aqf "name=jenkins-master")
 docker exec -u 0 -it $containerId mkdir /opt/deployment-files
@@ -35,5 +23,6 @@ docker pull shakedbraimok/example-app
 #--- Run container ---#
 docker run --name example-app -d --expose 8080 -p 127.0.0.1:8080:8080 -i -t shakedbraimok/example-app /bin/bash
 
-
+#--- Run Listener ---#
+python listener/listener.py &
 
